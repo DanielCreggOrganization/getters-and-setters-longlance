952c3eccc613564dac450f772d0d3b60dac07b2c
longlance <g00257419@gmit.ie>
2020-02-27 14:35:33 +0000

    person class

--- Main.java
+++ Main.java
@@ -1,3 +1,17 @@
 public class Main {
     // Main method
+    public static void main(String[] args) {
+        Person personObj1 = new Person();
+
+        personObj1.setfirstName("Fred");
+            System.out.println(personObj1.getfirstName());
+
+        personObj1.setmiddleName("Wilfred");
+            System.out.println(personObj1.getmiddleName());
+
+        personObj1 = new Person();
+            personObj1.setuserName("theRedRhino");//11 characters
+
+            System.out.println(personObj1.getuserName());
+    }
 }
--- Person.java
+++ Person.java
@@ -1,5 +1,56 @@
-class Person {
+public class Person {
     // Instance Variables
-    
+      private String firstName;
+      private String[] middleName;
+      private String userName; 
+
+
     // Methods
-}
+
+
+
+    
+    public String getfirstName(){
+        return firstName;
+
+    }
+
+    public void setfirstName(String firstName){
+        this.firstName = firstName;
+
+
+    }
+
+    public String getmiddleName(){
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < middleName.length; i++){
+            sb.append( middleName [i]+"");
+        }
+        String middleName = sb.toString();
+        return middleName;
+
+    }
+
+    public void setmiddleName(String middleName){
+        this.middleName = middleName.split("");
+        
+
+    }
+
+    public String getuserName(){
+        return userName;
+
+    }
+
+    public void setuserName(String userName) {
+        if (userName.length() > 10) {
+                this.userName = userName.substring(0,10);
+        }else{
+                this.userName = userName;
+             }       
+        }
+        
+
+    }
+
+
